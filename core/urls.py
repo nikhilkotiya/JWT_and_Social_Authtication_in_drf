c845351183ae38b3acac0b5fc344bf50acb1f396
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-11-08 17:01:32 +0530

    first commit

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 urlpatterns = [
     # Oauth
     path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
